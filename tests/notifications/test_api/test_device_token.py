0f1375069d4364d54d80e2f1f09e2a0fd0cb1989
dgis-demo <69810134+dgis-demo@users.noreply.github.com>
2023-03-26 13:02:54 +0200

    ADD: notifications api tests

--- tests/notifications/test_api/test_device_token.py
+++ tests/notifications/test_api/test_device_token.py
@@ -8,6 +8,7 @@ from apps.notifications.models import DeviceToken
 
 class TestDeviceToken:
     url = reverse('notifications:create-device-token')
+    details_url = 'notifications:details-device-token'
 
     @pytest.mark.django_db
     def test_list_success(self, api_client, device_token):
@@ -17,16 +18,39 @@ class TestDeviceToken:
         assert response.json()[0]['id'] == device_token.id
 
     @pytest.mark.django_db
-    # update, destroy
     def test_create_success(self, api_client, fake):
         response = api_client.post(
             self.url,
             data={
                 'device_id': fake.word(),
-                'device_type': 'IOS',
+                'device_type': DeviceToken.DeviceType.IOS,
                 'token': fake.word(),
             },
         )
 
         assert response.status_code == HTTPStatus.CREATED
         assert DeviceToken.objects.last().id == response.json()['id']
+
+    @pytest.mark.django_db
+    def test_update_success(self, api_client, device_token, fake):
+        response = api_client.patch(
+            reverse(self.details_url, kwargs={'pk': device_token.id}),
+            data={
+                'device_id': fake.word(),
+                'device_type': DeviceToken.DeviceType.ANDROID,
+                'token': fake.word(),
+            },
+        )
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json()['id'] == device_token.id
+        assert response.json()['device_type'] == DeviceToken.DeviceType.ANDROID
+
+    @pytest.mark.django_db
+    def test_destroy_success(self, api_client, device_token):
+        response = api_client.delete(
+            reverse(self.details_url, kwargs={'pk': device_token.id}),
+        )
+
+        assert response.status_code == HTTPStatus.NO_CONTENT
+        assert DeviceToken.objects.count() == 0
