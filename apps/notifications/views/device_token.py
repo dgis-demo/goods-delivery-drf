7c04daa0d1d3aff7b83d5e777f2664c8c421ed2c
dgis-demo <69810134+dgis-demo@users.noreply.github.com>
2023-03-26 12:47:48 +0200

    FIX: user agent parsing

--- apps/notifications/views/device_token.py
+++ apps/notifications/views/device_token.py
@@ -26,7 +26,7 @@ class DeviceTokenViewSet(
 
     def perform_create(self, serializer):
         user_agent = self.request._request.META.get('HTTP_USER_AGENT')
-        device = user_agent_parser.ParseDevice(user_agent)
+        device = self._get_device(user_agent)
         serializer.save(device=device, user=self.user)
 
     def perform_update(self, serializer):
@@ -38,3 +38,11 @@ class DeviceTokenViewSet(
     @property
     def user(self):
         return self.request.user
+
+    @staticmethod
+    def _get_device(user_agent: str) -> str:
+        device = ''
+        try:
+            device = user_agent_parser.ParseDevice(user_agent)
+        finally:
+            return device
--- tests/notifications/test_api/test_device_token.py
+++ tests/notifications/test_api/test_device_token.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 import pytest
 from django.shortcuts import reverse
 
+from apps.notifications.models import DeviceToken
+
 
 class TestDeviceToken:
     url = reverse('notifications:create-device-token')
@@ -13,3 +15,18 @@ class TestDeviceToken:
 
         assert response.status_code == HTTPStatus.OK
         assert response.json()[0]['id'] == device_token.id
+
+    @pytest.mark.django_db
+    # update, destroy
+    def test_create_success(self, api_client, fake):
+        response = api_client.post(
+            self.url,
+            data={
+                'device_id': fake.word(),
+                'device_type': 'IOS',
+                'token': fake.word(),
+            },
+        )
+
+        assert response.status_code == HTTPStatus.CREATED
+        assert DeviceToken.objects.last().id == response.json()['id']
